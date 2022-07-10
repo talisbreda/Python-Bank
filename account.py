8a3efe75a0a2a506ebe52c20358c6dbc8a82b64b
talisbreda <talis.breda@outlook.com>
2022-07-10 15:13:57 -0300

    Added comments to the code and optimized some functions

--- account.py
+++ account.py
@@ -10,21 +10,28 @@ class Account():
         self.client = client
         self.balance = 0
     
+    # Deposita um valor na conta
     def deposit(self):
         print("Insert the value of the deposit")
         value = float(input())
+        # Atualiza o saldo da conta no banco de dados
         self.balance = self.dbUpdate('deposit', int(value*100))
     
+    # Retira um valor na conta
     def withdraw(self):
         print("Insert the value of the withdrawal")
         value = float(input())
+        # Atualiza o saldo da conta no banco de dados
         self.balance = self.dbUpdate('withdraw', int(value*100))
     
+    # Transfere um valor para outra conta
     def transfer(self):
         print("Insert the value of the transfer")
         value = float(input())
         self.balance = self.dbUpdate('transfer', int(value*100))
-            
+    
+    # Função responsável por fazer atualizações no banco de dados, recebendo o valor da alteração
+    # e a operação a ser realizada
     def dbUpdate(self, transaction, value):
         with engine.connect() as conn:
             result = conn.execute(
@@ -35,11 +42,13 @@ class Account():
         balance = int(result.scalar())
         if transaction == 'deposit':
             balance += value
+            print("New balance: $%.2f" % (balance/100))
         if transaction == 'withdraw':
             if value > balance:
                 print("Insufficient balance")
             else:
                 balance -= value
+                print("New balance: $%.2f" % (balance/100))
         if transaction == 'transfer':
             if value > balance:
                 print("Insufficient balance")
@@ -48,18 +57,25 @@ class Account():
                 accNumber = int(input())
                 print("Insert the receiver's agency")
                 agency = int(input())
-                self.transferToReceiver(accNumber, agency, value)
-                balance -= value
-            # TODO
+                try:
+                    # Chama a função responsável por buscar a conta receptora no banco de dados e 
+                    # atualizar seu saldo
+                    self.transferToReceiver(accNumber, agency, value)
+                    balance -= value
+                    print("New balance: $%.2f" % (balance/100))
+                except: 
+                    print("Account not found")
+        
+        # Atualiza o saldo da conta instanciada
         with engine.connect() as conn:
             conn.execute(
                 text("UPDATE account SET balance = :balance WHERE idAccount = :id"),
                 [{"balance": balance, "id": self.id}]
             )
             conn.commit()
-        print("New balance: $%.2f" % (balance/100))
         return balance
     
+    # Função responsável por atualizar os dados da conta receptora de uma transferência
     def transferToReceiver(self, accNumber, agency, value):
         with engine.connect() as conn:
             result = conn.execute(
@@ -67,16 +83,20 @@ class Account():
                 [{"accNumber": accNumber, "agency": agency}]
             )
             conn.commit()
-
-        balance = self.testResult(result.scalar())
-        balance += value
-
-        with engine.connect() as conn:
-            conn.execute(
-                text("UPDATE account SET balance = :balance WHERE accNumber = :accNumber"),
-                [{"balance": balance, "accNumber": accNumber}]
-            )
-            conn.commit()
+        try:
+            # Se o resultado não for nulo, significa que a conta existe e a operação pode ser realizada
+            balance = self.testResult(result.scalar())
+            balance += value
+            with engine.connect() as conn:
+                conn.execute(
+                    text("UPDATE account SET balance = :balance WHERE accNumber = :accNumber"),
+                    [{"balance": balance, "accNumber": accNumber}]
+                )
+                conn.commit()
+        except:
+            # Caso o resultado seja nulo (a conta não existe), cria um erro a ser tratado 
+            # na função dbUpdate
+            raise Exception
         
     def testResult(self, result):
         if result is None:
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+# Imports de classe
 from account import Account
 from client import Client
+
+# Imports de bibliotecas
 from sqlalchemy import create_engine
 from sqlalchemy import text
 from random import randint
-from cryptography.fernet import Fernet
 from hashlib import sha3_256
+import re
 
+# Criação da engine do SQLAlchemy para acesso global ao banco de dados
 engine = create_engine("mysql+mysqlconnector://root:256984@localhost:3306/trabalho", echo=True, future=True)
-key = Fernet.generate_key()
-fernet = Fernet(key)
 
+# Cria um novo cliente no banco de dados com base em inputs do usuario
 def createNewClient():
     id = randint(100000, 999999)
+    # Inputs do usuário
     print("Please insert your name")
     fullname = input()
     print("Please insert your e-mail")
@@ -23,42 +27,56 @@ def createNewClient():
     print("Please insert your RG")
     rg = input()
     print("Please insert a password")
+    # Criptografia da senha inserida pelo usuário, usando SHA256
     passcode = sha3_256(input().encode('UTF-8')).hexdigest()
 
-    checkId(id)
+    # Verifica a unicidade do ID gerado
+    id = checkId(id, "client")
+    # Verifica se o email inserido é válido ou se já está registrado
     valid = checkEmail(email)
     
+    # Caso o email seja válido
     if valid:
+        # Cria uma conexão para inserir uma nova linha na tabela de clientes do banco de dados
         with engine.connect() as conn:
             conn.execute(
                 text("INSERT INTO client (idClient, passcode, fullname, email, phone, cpf, rg) VALUES (:id, :passcode, :fullname, :email, :phone, :cpf, :rg)"),
                 [{"id": id, "passcode": passcode, "fullname": fullname, "email": email, "phone": phone, "cpf": cpf, "rg": rg}]
             )
             conn.commit()
+        # Instancia uma classe com os dados do cliente
         client = Client(id, fullname, email, phone, cpf, rg, True)
+        # Acessa os serviços bancários na conta recém registrada
         bank(client)
     else:
+        # Caso o e-mail não seja válido ou já exista, a criação de usuário é reiniciada
         createNewClient()
 
+# Cria uma nova conta no banco de dados
 def createNewAccount(holder):
 
-    id = checkId(randint(100000, 999999))
+    # Geração de um ID e de um número de conta
+    id = checkId(randint(100000, 999999), "account")
     accNum = checkAcc(randint(100000, 999999))
-    
+
+    # Conexão para inserção de uma nova linha na tabela de contas do banco de dados
     with engine.connect() as conn:
         conn.execute(
             text("INSERT INTO account (idAccount, holder, accNumber, agency, accType, balance) VALUES (:id, :holder, :accNumber, :agency, :accType, :balance)"),
             [{"id": id, "holder": holder.getId(), "accNumber": accNum, "agency": 1, "accType": 1, "balance": 0}]
         )
         conn.commit()
+    # Retorna uma classe instanciada de Account
     return Account(holder, id)
 
+# Testa se o retorno do banco de dados é nulo, e retorna um erro caso for
 def testResult(result):
     if result is None:
         raise Exception("Credentials are incorrect")
     else:
         return result
-    
+
+# Realiza a autenticação de um usuário    
 def authenticate(email, password):
     with engine.connect() as conn:
         result = conn.execute(
@@ -67,30 +85,45 @@ def authenticate(email, password):
         )
         conn.commit()
     try:
+        # Testa o resultado da busca no banco de dados, e retorna um erro caso esta seja nula
         passcode = testResult(result.scalar())
+        # Criptografa a senha inserida e compara com a já existente no banco de dados
         encPassword = sha3_256(password.encode('UTF-8')).hexdigest()
         if encPassword == passcode:
             accessClient(email)
         else:
+            # Gera um erro caso algo as senhas sejam diferentes
             raise Exception("Credentials are incorrect")
     except Exception as e:
         print(e)
 
-
-def checkId(id):
+# Confere a unicidade do ID gerado
+def checkId(id, table):
     with engine.connect() as conn:
-        result = conn.execute(
-            text("SELECT idClient FROM client WHERE idClient = :id"),
-            [{"id": id}]
-        )
+        # Confere o ID de diferentes tabelas de acordo com o tipo de ID gerado
+        if table == "account":
+            result = conn.execute(
+                text("SELECT idAccount FROM account WHERE idAccount = :id"),
+                [{"id": id}]
+            )
+        else:
+            result = conn.execute(
+                text("SELECT idClient FROM client WHERE idClient = :id"),
+                [{"id": id}]
+            )
         conn.commit()
+    # Testa o resultado da busca para saber se é nulo
     try:
-        test = testResult(result.one())
+        # testResult retorna um erro caso o parâmetro seja nulo, o que significa que o
+        # ID não existe no banco de dados e a inserção pode continuar
+        test = testResult(result.scalar())
+        # result.scalar() retorna o primeiro elemento da primeira linha da chamada
         newid = randint(100000, 999999)
-        checkId(newid)
+        checkId(newid, table)
     except:
         return id
 
+# Checa a validade do e-mail e a sua existência no banco de dados
 def checkEmail(email):
     with engine.connect() as conn:
         result = conn.execute(
@@ -99,37 +132,51 @@ def checkEmail(email):
         )
         conn.commit()
     try:
-        test = testResult(result.one())
+        # Caso result não seja nulo, significa que o e-mail testado já está cadastrado
+        test = testResult(result.scalar())
         print("E-mail is already in use")
         return False
     except:
-        return True
-
+        try:
+            # Teste para validar a sintaxe do e-mail inserido
+            # Caso o e-mail não seja válido, re.match() retorna nulo, criando um erro.
+            test = testResult(re.match(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+            return True
+        except:
+            print("E-mail is not valid")
+            return False
     
-def checkAcc(id):
+# Checa a unicidade do número da conta gerado
+def checkAcc(accNum):
     with engine.connect() as conn:
         result = conn.execute(
-            text("SELECT idAccount FROM account WHERE idAccount = :id"),
-            [{"id": id}]
+            text("SELECT accNumber FROM account WHERE accNumber = :accNumber"),
+            [{"accNumber": accNum}]
         )
         conn.commit()
-    if result.scalar() != '':
-        id = randint(100000, 999999)
-        newid = checkId(id)
-        return newid
-    else:
-        return id
+    try:
+        test = testResult(result.scalar())
+        newAccNum = checkAcc(randint(100000, 999999))
+        return newAccNum
+    except:
+        return accNum
 
+# Após autenticação, é instanciada uma classe Client com as informações necessárias trazidas do banco 
+# de dados, para que se possa efetuar as operações necessárias
 def accessClient(email):
     with engine.connect() as conn:
         result = conn.execute(
             text("SELECT idClient, fullname, phone, cpf, rg FROM client WHERE email = :email"),
             [{"email": email}])
         conn.commit()
+    # result.first() retorna a primeira linha da chamada, que corresponde aos dados do cliente
     result = result.first()
+    # Instanciação da classe Client
     client = Client(result[0], result[1], email, result[2], result[3], result[4], False)
+    # Acesso aos serviços bancários a partir da conta do cliente
     bank(client)
 
+# Instanciação da classe Account para realização dos serviços
 def accessAccount(holder):
     with engine.connect() as conn:
         result = conn.execute(
@@ -140,6 +187,8 @@ def accessAccount(holder):
     return Account(holder, id)
 
 def bank(client):
+    # Se o cliente foi recém-criado, é necessário criar uma nova linha na tabela de contas do
+    # banco de dados
     if client.getNew() == False:
         acc = accessAccount(client)
     else:
@@ -164,6 +213,7 @@ def bank(client):
                 case 4:
                     break
         except ValueError: 
+            # Caso o erro ValueError aconteça, significa que um input inválido foi inserido
             print("Please insert a valid number")
 
 while True:
